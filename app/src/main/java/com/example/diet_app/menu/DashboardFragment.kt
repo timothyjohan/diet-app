6ac9620cf3b7303e30b803a949539f9052abcd9d
1maginaryNumbers <104255911+1maginaryNumbers@users.noreply.github.com>
2024-06-17 01:37:40 +0700

    navigate to settings

--- app/src/main/java/com/example/diet_app/menu/DashboardFragment.kt
+++ app/src/main/java/com/example/diet_app/menu/DashboardFragment.kt
@@ -5,23 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.diet_app.databinding.FragmentDashboardBinding
 
 
 class DashboardFragment : Fragment() {
     private lateinit var binding: FragmentDashboardBinding
+    private lateinit var email: String
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-    binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val navArgs: DashboardFragmentArgs by navArgs<DashboardFragmentArgs>()
-        val email = navArgs.email
-
-
+        email = navArgs.email.toString()
 
         return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btSettings.setOnClickListener(){
+            val action = DashboardFragmentDirections.actionDashboardFragmentToSettingsFragment(email)
+            findNavController().navigate(action)
+        }
     }
 
 }
