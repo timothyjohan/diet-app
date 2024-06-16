9fd996c80677e956dc94c26164224c8757853231
1maginaryNumbers <104255911+1maginaryNumbers@users.noreply.github.com>
2024-06-17 01:36:48 +0700

    Create Config.kt

---
+++ app/src/main/java/com/example/diet_app/Config.kt
@@ -0,0 +1,17 @@
+package com.example.diet_app
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class Config(private val context: Context) {
+    private val preferences: SharedPreferences =
+        context.getSharedPreferences("settings_prefs", Context.MODE_PRIVATE)
+
+    var dailyReminder: Boolean
+        get() = preferences.getBoolean("dailyReminder", false)
+        set(value) = preferences.edit().putBoolean("dailyReminder", value).apply()
+
+    var weeklyReminder: Boolean
+        get() = preferences.getBoolean("weeklyReminder", false)
+        set(value) = preferences.edit().putBoolean("weeklyReminder", value).apply()
+}
