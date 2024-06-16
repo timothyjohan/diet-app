cc4007ed6790f41b48e62b979e35cb790e2ee9e2
1maginaryNumbers <104255911+1maginaryNumbers@users.noreply.github.com>
2024-06-17 01:36:41 +0700

    save preferences in settings

--- app/src/main/java/com/example/diet_app/SettingsFragment.kt
+++ app/src/main/java/com/example/diet_app/SettingsFragment.kt
@@ -1,22 +1,31 @@
 package com.example.diet_app
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.diet_app.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
     lateinit var binding: FragmentSettingsBinding
-    var dailyReminder=false
-    var weeklyReminder=false
+    private lateinit var email: String
+    private lateinit var config: Config
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        val navArgs: SettingsFragmentArgs by navArgs<SettingsFragmentArgs>()
+        email = navArgs.email
+
+        config = Config(requireContext())
 
         return binding.root
     }
@@ -24,38 +33,18 @@ class SettingsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        if(dailyReminder == false){
-            binding.rbDailyOn.isChecked=true
-            binding.rbDailyOff.isChecked=false
-        }
-        else if(dailyReminder == true){
-            binding.rbDailyOff.isChecked=true
-            binding.rbDailyOn.isChecked=false
+        binding.swDaily.isChecked = config.dailyReminder
+        binding.swWeekly.isChecked = config.weeklyReminder
+
+        binding.btSaveSettings.setOnClickListener {
+            config.dailyReminder = binding.swDaily.isChecked
+            config.weeklyReminder = binding.swWeekly.isChecked
+            Toast.makeText(requireContext(), "Settings updated", Toast.LENGTH_SHORT).show()
         }
 
-        if(weeklyReminder == false){
-            binding.rbWeeklyOn.isChecked=true
-            binding.rbWeeklyOff.isChecked=false
-        }
-        else if(weeklyReminder == true){
-            binding.rbWeeklyOff.isChecked=true
-            binding.rbWeeklyOn.isChecked=false
-        }
-
-        binding.btSaveSettings.setOnClickListener(){
-            if(binding.rbDailyOn.isChecked){
-                dailyReminder=true
-            }
-            else if(binding.rbDailyOff.isChecked){
-                dailyReminder=false
-            }
-
-            if(binding.rbWeeklyOn.isChecked){
-                weeklyReminder=true
-            }
-            else if(binding.rbWeeklyOff.isChecked){
-                weeklyReminder=false
-            }
+        binding.btToDashboard.setOnClickListener {
+            val action = SettingsFragmentDirections.actionSettingsFragmentToDashboardFragment(email)
+            findNavController().navigate(action)
         }
     }
 
