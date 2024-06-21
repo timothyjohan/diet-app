5a34f65f40db88d5bf840990570a506389e905bb
lthreen <timothyjohan11@gmail.com>
2024-06-21 15:59:56 +0700

    BIsa dapet nutritions

---
+++ app/src/main/java/com/example/diet_app/data/source/NutritionResponse.kt
@@ -0,0 +1,14 @@
+package com.example.diet_app.data.source
+
+class NutritionResponse(
+    val food_name: String,
+    val serving_qty: Int,
+    val serving_unit: String,
+    val calories: Double,
+    val fats: Double,
+    val protein: Double,
+    val carbohydrate: Double,
+    val img: String
+) {
+
+}
